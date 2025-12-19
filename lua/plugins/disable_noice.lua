08efa02f6fffcebd201cbc4f3e11dbc0a307f3fa
Gabriel Castro <gaben24k@gmail.com>
2025-12-18 16:31:05 -0800

    add disable.lua

---
+++ lua/plugins/disable_noice.lua
@@ -0,0 +1,7 @@
+-- In your LazyVim plugins file
+return {
+  { "folke/noice.nvim", enabled = false },
+  { "lukas-reineke/indent-blankline.nvim", enabled = false },
+  { "L3MON4D3/LuaSnip", enabled = false },
+  { "rafamadriz/friendly-snippets", enabled = false },
+}
